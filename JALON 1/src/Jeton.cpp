7293eb013c12a47a002c4adb5eeb62eda4b6b4fd
Harrat Zohra <senouci12@hotmail.fr>
2018-05-18 08:57:43 +0200

    Mise à jour : 18/05/2018

--- JALON 1/src/Jeton.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Jeton.hpp"
-
-string getCouleurJeton()
-{
-	return couleur;
-}
-
--- JALON 1/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Plateau.hpp"
-int main() 
-{
-	Plateau plateau;
-	plateau.afficherPlateau();
-	return 0;
-}
-
